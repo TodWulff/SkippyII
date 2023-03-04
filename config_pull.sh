add521824dcd421bf38bba6907e03eba244fcf58
TodWulff <tod.wulff@gmail.com>
2023-03-04 14:37:39 -0500

    printer event - automated config backup

--- config_pull.sh
+++ config_pull.sh
@@ -1,7 +1,7 @@
 cd ~/printer_data/config
 git fetch
 echo "Pulling a config restore - will restart Klipper if config changes exist."
-echo 'M118 Checking config status.' > ~/printer_data/comms/klippy.serial
+# echo 'M118 Checking config status.' > ~/printer_data/comms/klippy.serial
 say_wait Checking for updated kunfihgs.
 
 if git status | grep -q 'Your branch is behind'; then
@@ -12,6 +12,6 @@ if git status | grep -q 'Your branch is behind'; then
 	echo RESTART > ~/printer_data/comms/klippy.serial
 else
 	echo "No changes exist, configs are up to date."
-	echo 'M118 No changes exist, configs are up to date.' > ~/printer_data/comms/klippy.serial
+#	echo 'M118 No changes exist, configs are up to date.' > ~/printer_data/comms/klippy.serial
 	say_wait No changes.
 fi

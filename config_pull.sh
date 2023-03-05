df0d1fa6c9e3bb6ca53122375531413b123c0b34
TodWulff <tod.wulff@gmail.com>
2023-03-04 19:35:37 -0500

    printer event - automated config backup

--- config_pull.sh
+++ config_pull.sh
@@ -5,13 +5,15 @@ echo "Pulling a config restore - will restart Klipper if config changes exist."
 say_wait Checking for updated kunfihgs.
 
 if git status | grep -q 'Your branch is behind'; then
-	git pull
+	git pull --ff-only
 	echo "Changes exist, pulled & restarting Klipper" 
 	echo 'M118 GIT Repo Pull RESTART' > ~/printer_data/comms/klippy.serial
-	say Changes pulled.  Restarting.
+	say_wait Changes pulled.
 	echo RESTART > ~/printer_data/comms/klippy.serial
 else
 	echo "No changes exist, configs are up to date."
 #	echo 'M118 No changes exist, configs are up to date.' > ~/printer_data/comms/klippy.serial
 	say_wait No changes.
+	echo 'UPDATE_DELAYED_GCODE ID=_advise_printer_state DURATION=0.1' > ~/printer_data/comms/klippy.serial
+
 fi

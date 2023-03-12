ddfe11fc598cd0b3825ca2e58e5bb379b1972deb
TodWulff <tod.wulff@gmail.com>
2023-03-11 22:30:31 -0500

    _another_unquoted_spaceless_string

--- config_push.sh
+++ config_push.sh
@@ -3,7 +3,7 @@ echo "Pushing a config backup."
 
 # echo '{action_respond_info("Pushing a config backup.")}' > ~/printer_data/comms/klippy.serial
 echo 'M118 Pushing a config backup.' > ~/printer_data/comms/klippy.serial
-say_wait  Pushing a kunfihg backup. 
+say_wait  Pushing a kunfihg backup.
 echo 'M118 Commit Message:' $@ > ~/printer_data/comms/klippy.serial
 
 cd ~/printer_data/config
@@ -11,7 +11,7 @@ git fetch && git status
 git add .
 git commit -m $@ 
 git push
-echo "Config backup complete
+echo "Config backup complete"
 
 #say  "Config backup complete."
 echo 'M118 Config backup complete.' > ~/printer_data/comms/klippy.serial
